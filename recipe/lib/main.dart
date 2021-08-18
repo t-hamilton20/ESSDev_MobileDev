f019f4abaad2241a7a160ab241410a082678dee4
Tom Hamilton <18tjwh@queensu.ca>
2021-08-17 20:15:16 -0400

    working input and multiplier!

--- recipe/lib/main.dart
+++ recipe/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-      theme: ThemeData(primaryColor: Colors.red, accentColor: Colors.redAccent),
+      theme: ThemeData(
+          primaryColor: Colors.blue[900], accentColor: Colors.blueAccent),
       home: new RecipeApp()));
 }
 
@@ -21,6 +22,7 @@ class _RecipeAppState extends State<RecipeApp> {
   int count = 0;
 
   var ingredients = List<Ingredient>.generate(10, (index) {
+    //generates list of ten ingredient objects
     return Ingredient(
       name: "",
       quantity: 0,
@@ -51,6 +53,7 @@ class _RecipeAppState extends State<RecipeApp> {
         ),
       ),
       body: new Padding(
+        //used to add space around content
         padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
         child: new Center(
           child: new Column(
@@ -59,15 +62,16 @@ class _RecipeAppState extends State<RecipeApp> {
               SizedBox(
                 height: 300,
                 child: new ListView.builder(
+                  //listview.builder creates a text box tile for every item in the ingredients list
                   itemCount: ingredients.length,
                   itemBuilder: (context, index) {
                     return ListTile(title: Text(ingredients[index].output()));
                   },
                 ),
               ),
-              new SizedBox(height: 20),
+              new SizedBox(height: 20), //added for spacing
               new TextField(
-                cursorColor: Colors.redAccent,
+                //textfield for ingredient input
                 decoration: new InputDecoration(hintText: "Input Ingredient"),
                 onSubmitted: (String ingredientsInput) {
                   setState(() {
@@ -75,9 +79,9 @@ class _RecipeAppState extends State<RecipeApp> {
                   });
                 },
               ),
-              new SizedBox(height: 10),
+              new SizedBox(height: 10), //added for spacing
               new TextField(
-                cursorColor: Colors.redAccent,
+                //textfield for amount input
                 decoration: new InputDecoration(hintText: "Input Amount"),
                 onSubmitted: (String amountsInput) {
                   setState(() {
@@ -85,9 +89,9 @@ class _RecipeAppState extends State<RecipeApp> {
                   });
                 },
               ),
-              new SizedBox(height: 10),
+              new SizedBox(height: 10), //added for spacing
               new TextField(
-                cursorColor: Colors.redAccent,
+                //textfield for unit input
                 decoration: new InputDecoration(
                     hintText: "Input Unit (ex: cups, tbsp, etc.)"),
                 onSubmitted: (String unitsInput) {
@@ -96,30 +100,35 @@ class _RecipeAppState extends State<RecipeApp> {
                   });
                 },
               ),
-              new SizedBox(height: 10),
+              new SizedBox(height: 10), //added for spacing
               new Slider(
+                //slider for multiplier input
                 value: multiplier,
                 min: 1,
                 max: 10,
-                divisions: 10,
-                inactiveColor: Colors.red,
-                activeColor: Colors.redAccent,
+                divisions: 9,
+                inactiveColor: Colors.blue[900],
+                activeColor: Colors.blueAccent,
                 label: multiplier.round().toString(),
                 onChanged: (double value) {
                   setState(() {
                     multiplier = value;
+                    for (int i = 0; i < ingredients.length; i++) {
+                      //iterate through each ingredient and set the multiplier to the inputted value
+                      ingredients[i].setMultiplier(multiplier);
+                    }
                   });
                 },
               ),
               ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.red),
+                style: ElevatedButton.styleFrom(primary: Colors.blue[900]),
                 child: Text("Add Ingredient"),
                 onPressed: () {
-                  ingredients[count].name = ingredient;
-                  ingredients[count].quantity = amount;
-                  ingredients[count].unit = unit;
+                  //when pressed, set the names, quantity, and unit for the current ingredient. then increment the counter and refresh
+                  ingredients[count].setName(ingredient);
+                  ingredients[count].setQuantity(amount);
+                  ingredients[count].setUnit(unit);
                   count++;
-                  print(count);
                   setState(() {});
                 },
               )
@@ -134,24 +143,54 @@ class _RecipeAppState extends State<RecipeApp> {
 class Ingredient {
   String name;
   double quantity;
+  double multiplier;
   String unit;
 
-  Ingredient({this.name = "", this.quantity = 0, this.unit = ""});
+  Ingredient(
+      {this.name = "", this.quantity = 0, this.multiplier = 1, this.unit = ""});
 
   String output() {
     if (name == "" && quantity == 0 && unit == "") {
       return "";
     } else {
-      return this.name +
+      return this.getName() +
           ": " +
-          this.quantity.toString() +
+          (this.getQuantity() * this.getMultiplier()).toString() +
           " " +
-          this.unit +
+          this.getUnit() +
           "(s)";
     }
   }
 
-  void multiply(double multiplier) {
-    this.quantity = quantity * multiplier;
+  void setName(String newName) {
+    this.name = newName;
+  }
+
+  void setQuantity(double newQuantity) {
+    this.quantity = newQuantity;
+  }
+
+  void setMultiplier(double newMultiplier) {
+    this.multiplier = newMultiplier;
+  }
+
+  void setUnit(String newUnit) {
+    this.unit = newUnit;
+  }
+
+  String getName() {
+    return name;
+  }
+
+  double getQuantity() {
+    return quantity;
+  }
+
+  double getMultiplier() {
+    return multiplier;
+  }
+
+  String getUnit() {
+    return unit;
   }
 }
