cc0a39d6db58e26a654deb6f69118fcc053fb6fd
Tom Hamilton <18tjwh@queensu.ca>
2021-09-23 15:01:06 -0400

    database stuff

--- Trivia_and_Users/trivia_game/lib/Database.dart
+++ Trivia_and_Users/trivia_game/lib/Database.dart
@@ -1,1 +1,9 @@
+import 'package:firebase_database/firebase_database.dart';
 
+final databaseReference = FirebaseDatabase.instance.reference();
+
+void createData(String username, int highscore) {
+  databaseReference
+      .child("$username")
+      .set({'name': '$username', 'highscore': '$highscore'});
+}
--- Trivia_and_Users/trivia_game/lib/main.dart
+++ Trivia_and_Users/trivia_game/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'Database.dart';
 
 class triviaApp extends StatefulWidget {
   const triviaApp({Key? key}) : super(key: key);
@@ -7,9 +9,29 @@ class triviaApp extends StatefulWidget {
   _triviaAppState createState() => _triviaAppState();
 }
 
+final databaseReference = FirebaseDatabase.instance.reference();
+
 class _triviaAppState extends State<triviaApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Trivia Game!"),
+        ),
+        body: new Padding(
+          padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
+          child: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [],
+            ),
+          ),
+        ));
   }
+}
+
+void createData(String username, int highscore) {
+  databaseReference
+      .child("$username")
+      .set({'name': '$username', 'highscore': '$highscore'});
 }
