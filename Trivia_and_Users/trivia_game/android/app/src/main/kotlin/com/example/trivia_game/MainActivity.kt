7fd2a4de8f4069b69c11fc5435996c6ccfd2601c
Tom Hamilton <18tjwh@queensu.ca>
2021-09-30 14:07:13 -0400

    Fresh start

--- Trivia_and_Users/trivia_game/android/app/src/main/kotlin/com/example/trivia_game/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trivia_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Trivia_and_Users/trivia_game/lib/Database.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_database/firebase_database.dart';
-
-final databaseReference = FirebaseDatabase.instance.reference();
-
-void createData(String username, int highscore) {
-  databaseReference
-      .child("$username")
-      .set({'name': '$username', 'highscore': '$highscore'});
-}
