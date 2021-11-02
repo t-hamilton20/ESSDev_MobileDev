99f777d8a0e48ad4457af4bbfc3ea95777578a45
Jwiggiff <friedman.josh03@gmail.com>
2021-11-02 14:57:40 -0400

    Add Firebase Initialization

--- Project/homeslice/lib/main.dart
+++ Project/homeslice/lib/main.dart
@@ -1,6 +1,6 @@
-//import 'dart:html';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:homeslice/connection_error.dart';
 import 'package:homeslice/login.dart';
 import 'package:homeslice/signup.dart';
 import 'package:homeslice/setup.dart';
@@ -8,22 +8,48 @@ import 'package:homeslice/setup.dart';
 // This code runs the app and navigates between pages
 
 void main() {
-  String theme = "dark";
-  runApp(new MaterialApp(
-      routes: {
-        '/login': (context) => login(),
-        '/signup': (context) => signup(),
-        //'/swiping': (context) => homeSwipe(),
-        //'/setup': (context) => setup()
-      },
-      theme: ThemeData(
-          primaryColor: Colors.grey[1000],
-          secondaryHeaderColor: Colors.grey[750],
-          brightness: Brightness.dark),
-      home: new login()));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class homeSwipe extends StatelessWidget {
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasError) {
+          return ConnectionError();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return new MaterialApp(
+            routes: {
+              '/login': (context) => Login(),
+              '/signup': (context) => Signup(),
+              '/swiping': (context) => HomeSwipe(),
+              '/setup': (context) => Setup()
+            },
+            home: new Login(),
+          );
+        }
+
+        return Loading();
+      },
+    );
+  }
+}
+
+class HomeSwipe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
