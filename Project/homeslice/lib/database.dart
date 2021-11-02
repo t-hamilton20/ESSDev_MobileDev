a4896cfd2bde752db0a2664e2f202ac5d8090cdc
Jwiggiff <friedman.josh03@gmail.com>
2021-11-02 15:39:04 -0400

    Add addUser and getUsers functions

--- Project/homeslice/lib/database.dart
+++ Project/homeslice/lib/database.dart
@@ -10,3 +10,45 @@
 * Delete_Group
 * Like_User
 */
+
+import "package:firebase_auth/firebase_auth.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+Future<void> addUser(name, email, minHousemates, maxHousemates, minPrice,
+    maxPrice, coed, minDist, maxDist) {
+  return firestore.collection("users").add({
+    'full_name': name,
+    'email': email,
+    'minHousemates': minHousemates,
+    'maxHousemates': maxHousemates,
+    'minPrice': minPrice,
+    'maxPrice': maxPrice,
+    'minDist': minDist,
+    'maxDist': maxDist,
+    'coed': coed,
+    'likedUsers': [],
+    'matchedUsers': []
+  });
+}
+
+Future<QuerySnapshot> getUsers(currentUserID) async {
+  Map<String, dynamic> currentUser =
+      (await firestore.collection("users").doc(currentUserID).get()).data()
+          as Map<String, dynamic>;
+
+  return firestore
+      .collection("users")
+      .where("minHousemates",
+          isGreaterThanOrEqualTo: currentUser['minHousemates'])
+      .where("minHousemates", isLessThanOrEqualTo: currentUser['maxHousemates'])
+      .where("minPrice", isGreaterThanOrEqualTo: currentUser['minPrice'])
+      .where("minPrice", isLessThanOrEqualTo: currentUser['maxPrice'])
+      .where("minDist", isGreaterThanOrEqualTo: currentUser['minDist'])
+      .where("minDist", isLessThanOrEqualTo: currentUser['maxDist'])
+      .where("coed", isEqualTo: currentUser['coed'])
+      .orderBy("name")
+      .limit(50)
+      .get();
+}
