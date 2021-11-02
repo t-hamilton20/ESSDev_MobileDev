ed2978c25ce72b282ede082ffeab9ebbda3fee8a
Tom Hamilton <18tjwh@queensu.ca>
2021-11-02 14:43:32 -0400

    test

--- Project/homeslice/lib/login.dart
+++ Project/homeslice/lib/login.dart
@@ -6,3 +6,19 @@
 * Forgot_Password
 * Send_to_Signup
 */
+
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
