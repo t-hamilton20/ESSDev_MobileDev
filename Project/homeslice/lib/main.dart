aae0322e2441db7b0a28d053fb804418b590a928
Tom Hamilton <18tjwh@queensu.ca>
2021-11-09 15:54:11 -0500

    working web app support

--- Project/homeslice/lib/main.dart
+++ Project/homeslice/lib/main.dart
@@ -20,30 +20,33 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _initialization,
+      future: Firebase.initializeApp(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.hasError) {
+          print("SNAPSHOT ERROR: " + snapshot.error.toString());
           return ConnectionError();
         }
 
         if (snapshot.connectionState == ConnectionState.done) {
           return new MaterialApp(
+            theme: ThemeData(
+                primaryColor: Colors.grey[1000],
+                secondaryHeaderColor: Colors.grey[750],
+                brightness: Brightness.dark),
             routes: {
               '/login': (context) => Login(),
               '/signup': (context) => Signup(),
-              '/swiping': (context) => HomeSwipe(),
-              '/setup': (context) => Setup()
+              //'/swiping': (context) => HomeSwipe(),
+              //'/setup': (context) => Setup()
             },
             home: new Login(),
           );
         }
-
-        return Loading();
+        return new Container();
+        //return Loading();
       },
     );
   }
