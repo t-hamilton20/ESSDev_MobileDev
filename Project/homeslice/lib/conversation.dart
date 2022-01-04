8977089c5b83158487a474911e12eef66a359106
Tom Hamilton <18tjwh@queensu.ca>
2022-01-04 11:12:02 -0500

    added timestamps

--- Project/homeslice/lib/conversation.dart
+++ Project/homeslice/lib/conversation.dart
@@ -20,11 +20,11 @@ class Conversation extends StatefulWidget {
 
 class _ConversationState extends State<Conversation> {
   var messages = [
-    new Message(messageText: "Hi", time: "9:00", type: "receiver"),
-    new Message(messageText: "Hi there", time: "9:05", type: "sender"),
+    new Message(messageText: "Hi", time: "9:00 PM", type: "receiver"),
+    new Message(messageText: "Hi there", time: "9:05 PM", type: "sender"),
     new Message(
         messageText: "Do you want to live with me",
-        time: "9:00",
+        time: "10:00 PM",
         type: "receiver"),
     new Message(
         messageText: "No you are in commerce", time: "9:00", type: "sender"),
@@ -109,7 +109,11 @@ class _ConversationState extends State<Conversation> {
                       width: 15,
                     ),
                     Expanded(
+                      // Input text field
                       child: TextField(
+                        keyboardType: TextInputType.multiline,
+                        minLines: 1,
+                        maxLines: 6,
                         cursorColor: Theme.of(context).hintColor,
                         decoration: InputDecoration(
                             hintText: "Write message...",
@@ -159,12 +163,18 @@ class _ConversationState extends State<Conversation> {
   }
 }
 
+// Message class for each individual message
 class Message extends StatefulWidget {
   String messageText;
   String time;
-  String type;
+  String type; // sender or receiver
+  bool displayTime;
 
-  Message({required this.messageText, required this.time, required this.type});
+  Message(
+      {required this.messageText,
+      required this.time,
+      required this.type,
+      this.displayTime = false});
 
   @override
   _MessageState createState() => _MessageState();
@@ -173,25 +183,64 @@ class Message extends StatefulWidget {
 class _MessageState extends State<Message> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+    return GestureDetector(
+      onTap: () {
+        widget.displayTime = !widget.displayTime;
+        setState(() {});
+        print(widget.displayTime);
+      },
       child: Align(
         alignment: (widget.type ==
                 "receiver" // if type is receiver alignment goes in top left
             ? Alignment.topLeft
             : Alignment.topRight),
         child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: (widget.type ==
-                    "receiver" // if type is receiver colour is light grey
-                ? Colors.grey[700]
-                : Colors.grey[900]),
-          ),
-          padding: EdgeInsets.all(16),
-          child: Text(
-            widget.messageText,
-            style: TextStyle(fontSize: 15),
+          padding: EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 5),
+          child: Column(
+            children: [
+              // container around message
+              Align(
+                alignment: (widget.type ==
+                        "receiver" // if type is receiver alignment goes in top left
+                    ? Alignment.topLeft
+                    : Alignment.topRight),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: (widget.type ==
+                            "receiver" // if type is receiver colour is light grey
+                        ? Colors.grey[700]
+                        : Colors.grey[900]),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  // actual message text
+                  child: Text(
+                    widget.messageText,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              // used to display time
+              Visibility(
+                visible: widget.displayTime,
+                child: Align(
+                  alignment: (widget.type ==
+                          "receiver" // if type is receiver alignment goes in top left
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      widget.time,
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
