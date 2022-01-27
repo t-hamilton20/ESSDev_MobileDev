f60a938cd311109a0ac6a3879c5fd47649b3fb48
tryagain50 <nat.kuk22@gmail.com>
2022-01-26 19:26:13 -0500

    Added draft of profile/settings page

--- Project/homeslice/lib/settings.dart
+++ Project/homeslice/lib/settings.dart
@@ -9,3 +9,67 @@
 * Remove_Account
 * Delete_Account
 */
+
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:homeslice/home_swipe.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    // User
+    User? user = Provider.of<User?>(context);
+    String? profileImg = user!.photoURL;
+    String myName = user.displayName.toString();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          //profile image or little portrait icon
+          (profileImg != null)
+              ? Image.network(
+                  profileImg,
+                  width: 200.0,
+                  height: 200.0,
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topCenter,
+                )
+              : Icon(Icons.portrait),
+
+          //user's name
+          Text(myName),
+
+          //Edit Profile Info - goes to setup
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                Navigator.pushNamed(context, '/setup');
+              });
+            },
+            child: const Text('Edit Profile Info'),
+          ),
+
+          //Edit Account Details - goes to Settings
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                Navigator.pushNamed(context, '/setup');
+              });
+            },
+            child: const Text('Edit Account Details'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//Widget 
